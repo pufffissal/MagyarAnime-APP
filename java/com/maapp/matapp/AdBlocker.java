f3e10912116a4c02292d7bcb3ba458943bd08b7b
pufffissal <139345254+pufffissal@users.noreply.github.com>
2025-06-23 08:47:53 +0200

    Update AdBlocker.java

--- java/com/maapp/matapp/AdBlocker.java
+++ java/com/maapp/matapp/AdBlocker.java
@@ -7,7 +7,7 @@ public class AdBlocker {
     private static final List<String> adUrls = Arrays.asList(
             "doubleclick.net", "ads.", "googlesyndication.com", "adservice.google.",
             "amazon-adsystem.com", "moatads.com", "scorecardresearch.com"
-    );
+    ); //Ha a MA frissítni a reklámjait, addj hozzá több d.
 
     public static boolean isAd(String url) {
         for (String ad : adUrls) {
